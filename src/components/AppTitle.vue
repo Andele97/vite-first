dfeedea5550a4e5d46c12f6820aa3573a52b0e4a
Andele97 <andreapinna40@live.it>
2022-11-14 15:11:48 +0100

    finish

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+  <script>
+  export default {
+    name: 'AppTitle',
+    data(){
+      return{
+        titolo: 'La mia prima app con Vite!'
+      }
+    }
+  }
+  </script>
+
+<template>
+  <div>
+    <h2>{{titolo}}</h2>
+  </div>
+</template>
+
+
+<style scoped>
+  h2 {
+    color: green;
+  }
+</style>
